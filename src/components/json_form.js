85adbd77808ef1d7d313f3b21aeb5dcf5736367f
Abijit <abijit@Abijits-MacBook-Air.local>
2022-10-17 21:35:51 +0545

    displaying inputs in console

--- src/components/json_form.js
+++ src/components/json_form.js
@@ -2,22 +2,31 @@ import React from 'react'
 import data from './json_vehicledata'
 
 export default function json_form(){
+	function onSubmit(e){
+		e.preventDefault();
+		data.topics.map(data1 => {
+			data1.properties.map(i => {
+				var ids = document.getElementById(i.label).value;
+				console.log(ids);
+			})
+		})
+	}
 	return (
 		<div className="container">
-		<form className="form-outer">
+		<form className="form-outer" onSubmit = {onSubmit}>
 			{data.topics.map(vdata => {
-				console.log(vdata)
+				//console.log(vdata)
 				return(
 					<div>
 				<h3>{vdata.title}</h3>
 				<h5>{vdata.description}</h5>
 				{
 					vdata.properties.map(input => {
-						console.log(input)
+						//console.log(input)
 						return(
 							<div className="form-group">
 							<label>{input.label}</label>
-							<input type={input.html_element} className="form-control"/>
+							<input id = {input.label} type={input.html_element} className="form-control"/>
 							</div>
 							)
 					})
@@ -27,7 +36,7 @@ export default function json_form(){
 				)
 			})}
 
-					<button type = "onSubmit" className="btn btn-primary"> Submit </button>
+					<button type = "{onSubmit}" className="btn btn-primary"> Submit </button>
 			</form>
 		</div>
 		)
